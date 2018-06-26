f7170c2abd60c122d5526ffae6103f65e7f02a96
Bram Esendam <bram.esendam@gmail.com>
2018-06-26 14:17:36 +0200

    done

--- BotRestarter/BotRestarter/Program.cs
+++ BotRestarter/BotRestarter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,24 @@ namespace BotRestarter
 {
     public class Program
     {
-        static void Main(string[] args)
+        private static void Main()
+            => new Program().StartAsync().GetAwaiter().GetResult();
+
+        public async Task StartAsync()
         {
+            while (true)
+            {
+                try
+                {
+                    Process.Start("ColorChan.lnk");
+                    await Task.Delay(1797500);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
         }
     }
 }
