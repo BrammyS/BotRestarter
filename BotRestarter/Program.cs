b3c83450f93cc270acaea1a9abc7811c37758aff
BramEsendam <bram.esendam@gmail.com>
2019-01-13 17:37:41 +0100

    Added new restarter to support .Net core apps

--- BotRestarter/Program.cs
+++ BotRestarter/Program.cs
@@ -1,61 +1,102 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BotRestarter
 {
     public class Program
     {
-        private string[] filePaths;
+
         private static void Main()
             => new Program().StartAsync().GetAwaiter().GetResult();
 
         private async Task StartAsync()
         {
+            var botFilePaths = GetFiles();
+
+            if (!botFilePaths.Any())
+            {
+                await ShowErrorMessageAsync("No bots where found!").ConfigureAwait(false);
+                return;
+            } 
+
+            // Loop trough all the bots and start them.
+            foreach (var bot in botFilePaths)
+            {
+                var thread = new Thread(() => StartBot(bot));
+                thread.Start();
+            }
+
+            await Task.Delay(-1).ConfigureAwait(false);
+        }
+
+
+        /// <summary>
+        /// Starts a bot with the provided file path.
+        /// And it keeps restarting the bot if it closes.
+        /// </summary>
+        /// <param name="botFilePath">The filepath of the bot that will be started.</param>
+        private void StartBot(string botFilePath)
+        {
+
             while (true)
             {
-                try
+                var botProcess = new Process
                 {
-                    GetFiles();
-                    StartPrograms();
-                    if (Config.ConfigData.RestartTime < 10)
+                    StartInfo = new ProcessStartInfo
                     {
-                        Console.WriteLine("FFHKJFHSKFSHFKUAHYUSIDHKJOZIYZ");
-                        await Task.Delay(-1);
+                        CreateNoWindow = false,
+                        WindowStyle = ProcessWindowStyle.Minimized,
+                        FileName = botFilePath,
+                        UseShellExecute = true
                     }
-                    await Task.Delay(Config.ConfigData.RestartTime * 1000 * 60);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                    throw;
-                }
-            }
-        }
-        private void GetFiles()
-        {
-            filePaths = Directory.GetFiles("bots", "*.lnk", SearchOption.AllDirectories);
-            foreach (var file in filePaths)
-            {
-                Console.WriteLine($"{DateTime.Now:G} : Found {file}!");
+                };
+                botProcess.Start();
+                botProcess.WaitForExit();
             }
         }
 
-        private void StartPrograms()
+
+        /// <summary>
+        /// Loads all the bot file paths in the bots folder.
+        /// </summary>
+        /// <returns>
+        /// A array of strings containing the file paths.
+        /// </returns>
+        private string[] GetFiles()
         {
-            Console.WriteLine($"{DateTime.Now:G} : Restart time = {Config.ConfigData.RestartTime} minuten ({Config.ConfigData.RestartTime * 1000 * 60}ms)");
-            foreach (var file in filePaths)
-            {
-                Console.WriteLine($"{DateTime.Now:G} : Starting {file}");
-                var startInfo = new ProcessStartInfo
-                {
-                    CreateNoWindow = false,
-                    WindowStyle = ProcessWindowStyle.Minimized,
-                    FileName = file
-                };
-                Process.Start(startInfo);
-            }
+            var botFilePaths = Directory.GetFiles("bots", "*.lnk", SearchOption.AllDirectories);
+            foreach (var file in botFilePaths) Log($"Found {file}!");
+            return botFilePaths;
+        }
+
+
+        /// <summary>
+        /// Logs a message to the console with a red color.
+        /// </summary>
+        /// <param name="message">The message that will be printed to the console.</param>
+        private async Task ShowErrorMessageAsync(string message)
+        {
+            Log(message, ConsoleColor.Red);
+            await Task.Delay(30000).ConfigureAwait(false);
+        }
+
+
+        /// <summary>
+        /// Logs a message to the console.
+        /// The <paramref name="color"/> is by default <see cref="ConsoleColor.Gray"/>
+        /// </summary>
+        /// <param name="message">The message that will be printed to the console.</param>
+        /// <param name="color">The color that the message will have when printing something to the console.</param>
+        private void Log(string message, ConsoleColor color = ConsoleColor.Gray)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine($"{DateTime.Now:hh:mm:ss.fff} : " + message);
+            Console.ResetColor();
         }
     }
 }
