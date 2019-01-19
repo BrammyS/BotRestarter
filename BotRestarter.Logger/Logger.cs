d4730fca45130544c9d3ff07c4ebb73cc6c57d89
BramEsendam <bram.esendam@gmail.com>
2019-01-19 20:09:14 +0100

    Added implementation for logger

---
+++ BotRestarter.Logger/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DiscordBotRestarter.Logger.Interfaces;
+
+namespace DiscordBotRestarter.Logger
+{
+    public class Logger : ILogger
+    {
+
+
+        /// <inheritdoc />
+        public void Log(string message, ConsoleColor color = ConsoleColor.Gray)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine($"{DateTime.Now:hh:mm:ss.fff} : " + message);
+            Console.ResetColor();
+        }
+    }
+}
