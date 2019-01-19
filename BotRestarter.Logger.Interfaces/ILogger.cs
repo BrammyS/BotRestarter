a2810508bd85f8482481a9ba45c820bb901ca76f
BramEsendam <bram.esendam@gmail.com>
2019-01-19 20:08:44 +0100

    Added logger to a separate project

---
+++ BotRestarter.Logger.Interfaces/ILogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DiscordBotRestarter.Logger.Interfaces
+{
+    public interface ILogger
+    {
+
+
+        /// <summary>
+        /// Logs a message to the console.
+        /// The <paramref name="color"/> is by default <see cref="ConsoleColor.Gray"/>
+        /// </summary>
+        /// <param name="message">The message that will be printed to the console.</param>
+        /// <param name="color">The color that the message will have when printing something to the console.</param>
+        void Log(string message, ConsoleColor color = ConsoleColor.Gray);
+    }
+}
