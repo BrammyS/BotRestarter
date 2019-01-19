74f347185c4bc840ad27d177ff65b24601ab4aab
BramEsendam <bram.esendam@gmail.com>
2019-01-19 20:16:58 +0100

    Added BotRestarter.Interfaces project

---
+++ BotRestarter.Interfaces/IBotRestarter.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace BotRestarter.Interfaces
+{
+    public interface IBotRestarter
+    {
+
+
+        /// <summary>
+        /// Starts all the methods to start all the bots and all the methods to keep them alive.
+        /// </summary>
+        Task StartAsync();
+    }
+}
---
+++ BotRestarter/BotRestarter.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using BotRestarter.Interfaces;
+
+namespace BotRestarter
+{
+    public class BotRestarter : IBotRestarter
+    {
+        public Task StartAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
